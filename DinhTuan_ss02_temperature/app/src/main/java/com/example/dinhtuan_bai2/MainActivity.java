c129190c0ca1ffcabab0e2c8148db7a526a974ce
vodinhtuan <vodinhtuan174@gmail.com>
2022-09-02 23:04:34 +0700

    update

---
+++ DinhTuan_ss02_temperature/app/src/main/java/com/example/dinhtuan_bai2/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.dinhtuan_bai2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    // Khai báo các biến giao diện:
+    EditText edtF, edtC;
+    Button btnCF, btnFC, btnClear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Ánh xạ id:
+        edtC = findViewById(R.id.edtC);
+        edtF = findViewById(R.id.edtF);
+        btnCF = findViewById(R.id.btnCF);
+        btnFC = findViewById(R.id.btnFC);
+        btnClear = findViewById(R.id.btnClear);
+        // Xử lý click:
+        btnCF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DecimalFormat dcf = new DecimalFormat("0.00");
+                int C = Integer.parseInt(edtC.getText().toString());
+                double F = C*1.8 + 32;
+                edtF.setText(dcf.format(F)+"");
+
+            }
+        });
+        btnFC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DecimalFormat dcf = new DecimalFormat("0.00");
+                int F = Integer.parseInt(edtF.getText().toString());
+                double C = (F-32)/1.8;
+                edtC.setText(dcf.format(C)+"");
+
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edtC.setText("");
+                edtF.setText("");
+            }
+        });
+    }
+}
---
+++ DinhTuan_ss03_calendar/app/src/main/java/com/example/dinhtuan_ss03_calendar/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.dinhtuan_ss03_calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    // Khai báo các biến giao diện:
+    EditText edtCalendar;
+    Button btnConvert;
+    TextView txtLunar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Ánh xạ id:
+        edtCalendar = findViewById(R.id.edtCalendar);
+        btnConvert = findViewById(R.id.btnConvert);
+        txtLunar = findViewById(R.id.txtLunar);
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 String can = " ", chi = " ";
+                 int namDuong = Integer.parseInt(edtCalendar.getText().toString());
+                 switch (namDuong%10){
+                     case 0: can = "Canh "; break;
+                     case 1: can = "Tân "; break;
+                     case 2: can = "Nhâm "; break;
+                     case 3: can = "Quý "; break;
+                     case 4: can = "Giáp "; break;
+                     case 5: can = "Ất "; break;
+                     case 6: can = "Bính "; break;
+                     case 7: can = "Đinh "; break;
+                     case 8: can = "Mậu "; break;
+                     case 9: can = "Kỷ"; break;
+                 }
+                switch (namDuong%12){
+                    case 0: chi = "Thân "; break;
+                    case 1: chi = "Dậu "; break;
+                    case 2: chi = "Tuất "; break;
+                    case 3: chi = "Hợi "; break;
+                    case 4: chi = "Tý "; break;
+                    case 5: chi = "Sửu "; break;
+                    case 6: chi = "Dần "; break;
+                    case 7: chi = "Mẹo "; break;
+                    case 8: chi = "Thìn "; break;
+                    case 9: chi = "Tỵ "; break;
+                    case 10: chi = "Ngọ "; break;
+                    case 11: chi = "Mùi "; break;
+                }
+                txtLunar.setText(can + chi);
+            }
+        });
+
+    }
+}
