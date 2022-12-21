300ac807dc91059aae44e224e352bb375af0fbba
vodinhtuan <vodinhtuan174@gmail.com>
2022-12-21 10:58:27 +0700

    update

--- DinhTuan_sqlProject/app/src/main/java/com/example/dinhtuan_sqlproject/MainActivity.java
+++ DinhTuan_sqlProject/app/src/main/java/com/example/dinhtuan_sqlproject/MainActivity.java
@@ -9,14 +9,7 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    // Khai báo các biến liên quan đến DataBase
-    String DB_PATH_SUFFIX = "/databases/";      // Thư mục mặc định chứa cơ sở dữ liệu trong cache điện thoại
-    SQLiteDatabase database = null;             // Khai báo tên cơ sở dữ liệu
-    String DATABASE_NAME = "Book.db";           // Tên file chứa dataBase
 
-    // Khai báo listView
-    ListView lv;
-    ArrayList<String>
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
